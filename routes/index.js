5305714b26c38ad7c3c51d8712d3cdfed1b87dbe
na0ya <na0ya.scorpio@gmail.com>
2016-11-03 01:22:57 +0900

    Create a sample API call using jQuery from client side

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', apikey: process.env.GNAVI_API_KEY });
 });
 
 module.exports = router;
